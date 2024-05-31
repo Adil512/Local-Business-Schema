e426a95ccc7e8beb111a073d33c59c48d8842a38
Muhammad Adil <alijee2929@gmail.com>
2024-05-31 17:12:18 +0400

    local-business-schema.js

---
+++ local-business-schema.js
@@ -0,0 +1,33 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type" : "LocalBusiness",
+  "@id" : "WEBSITE URL",
+  "name" : "BUSINESS NAME",
+  "hasMap": "https://maps.google.com/maps?cid=YOURCIDNUMBER",
+  "logo" : "LOGO URL",
+  "telephone" : "PHONE NUMBER",
+  "email" : "EMAIL ADDRESS",
+  "sameAs" : [ "URL #1", "URL #2" ],
+  "url" : "WEBSITE URL",
+  "image" : "IMAGE URL",
+  "priceRange" : "$000 - $000",
+  "description" : "BUSINESS DESCRIPTION",
+  "address" : {
+    "@type" : "PostalAddress",
+    "streetAddress" : "STREET ADDRESS",
+    "addressLocality" : "CITY",
+    "addressRegion" : "STATE ABBREVIATION",
+    "postalCode" : "ZIP CODE"
+  },
+  "geo" : {
+    "@type" : "GeoCoordinates",
+    "latitude" : "LATITUDE COORDINATES",
+    "longitude" : "LONGITUDE COORDINATES"
+  }
+        }
+    }
+  ]
+}
+</script>
+//Created by Adil Balti (baltibloggers.com)
